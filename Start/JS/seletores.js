6506fd1ca78c5a8c2b8aaab75f33d97d566cc4f6
Ricardo Borges de Faria Filho <ricardoborgesdefariadev@gmail.com>
2022-11-26 01:23:59 -0300

    update seletores.html

--- Start/JS/seletores.js
+++ Start/JS/seletores.js
@@ -7,9 +7,18 @@
 // // querySelector
 // // querySelectorAll (Nodelist)
 
-const header = document.querySelector('header')
-header.setAttribute('id', 'header')
+// const header = document.querySelector('header')
+// header.setAttribute('id', 'header')
 
-const headerID = document.querySelector('#header')
+// const headerID = document.querySelector('#header')
 
-header.removeAttribute('id','class')
+// header.removeAttribute('id','class')
+
+// como alterar estilos com a classe
+
+const element = document.querySelector('body')
+
+element.style.backgroundColor = 'green'
+
+
+console.log(element.style.backgroundColor);
