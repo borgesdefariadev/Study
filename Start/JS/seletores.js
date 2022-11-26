1f0e552d2ec744a696f337cd0fb5d7276fad0072
Ricardo Borges de Faria Filho <ricardoborgesdefariadev@gmail.com>
2022-11-25 22:08:50 -0300

    Update seletores.js

---
+++ Start/JS/seletores.js
@@ -0,0 +1,15 @@
+
+// const elements = document.querySelectorAll()
+
+// // getElementById
+// // getElementBytagName
+// // getElementByClassName (HTML5 Collection)
+// // querySelector
+// // querySelectorAll (Nodelist)
+
+const header = document.querySelector('header')
+header.setAttribute('id', 'header')
+
+const headerID = document.querySelector('#header')
+
+header.removeAttribute('id','class')
