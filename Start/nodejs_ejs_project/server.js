698a81e6d22111c89ebe4212d5b5924119e4c6aa
Ricardo Borges de Faria Filho <ricardoborgesdefariadev@gmail.com>
2022-12-07 16:22:32 -0300

    update

--- Start/nodejs_ejs_project/server.js
+++ Start/nodejs_ejs_project/server.js
@@ -4,7 +4,27 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('pages/index')
+    const items =[
+        {
+            title: 'D',
+        message: 'Desenvolver aplicaçoes/serviços de forma facil'
+        },
+        {
+            title: 'M',
+        message: 'Mirror Breaks after you looking on it'
+
+        },
+        {
+            title: 'D',
+        message: 'Demons flaying souls after getting beat up'
+
+        },
+    ];
+
+    res.render('pages/index', {
+        qualities: items,
+    });
+   
 })
 
 app.get('/about', (req, res) => {
