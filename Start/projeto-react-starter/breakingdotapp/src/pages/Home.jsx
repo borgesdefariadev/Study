6e887d152c8878bce0cc99b505074ab09ea1e95d
Ricardo Borges <ricardoborgesdefariadev@gmail.com>
2022-12-27 17:46:15 -0300

    update

---
+++ Start/projeto-react-starter/breakingdotapp/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+
+  return (
+    <div>
+    <h1>Lista da reunião</h1>
+    <input type="text" placeholder="Digite o nome..." />
+    <button type="button">Adicionar membro</button>
+    </div>
+  )
+}
+
+export default Home;
